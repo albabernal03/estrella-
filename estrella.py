a4e23752c6e43215f21ca27de1c7858dc16422a4
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-20 19:32:42 +0100

    el problema estaba en el range

--- estrella.py
+++ estrella.py
@@ -3,7 +3,10 @@ import turtle
 #Para mostrar el puntero
 
 turtle.showturtle()
-
-longitud= 50
+longitud=100
 angulo=160
+
+for i in range (10):
+    turtle.forward(longitud)
+    turtle.right(angulo)
 
